45117ead6eadc14743bdc125696d8c93533c2db7
kharshi14 <kesaraharshi2006@gmail.com>
2025-08-19 15:21:31 +0530

    Fix: added title to error handler

--- app.js
+++ app.js
@@ -28,10 +28,16 @@ app.use(function(req, res, next) {
 });
 
 // error handler
+// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error', {
+    title: 'Error',       // ✅ Add this line
+    message: err.message,
+    error: err
+  });
+});
+
 
   // render the error page
   res.status(err.status || 500);
